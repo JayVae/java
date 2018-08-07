9a59c2630f33e6c2282be7fc8020a88c75841503
JayVae <919433524@qq.com>
2018-08-07 21:18:19 +0800

    jdk集合源码

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ArrayList list = new ArrayList();
+        ArrayList<Integer> ints = new ArrayList<Integer>(Arrays.asList(0, 1, 2,
+                3, 4, 5, 6));
+        System.out.println(Arrays.toString(ints.toArray()));
     }
 }
