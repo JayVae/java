b6c7e1bfb7d15d667c1933f99b39e5233c3afb0a
JayVae <919433524@qq.com>
2018-08-08 17:01:34 +0800

    jdk

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Main {
 
@@ -9,5 +10,13 @@ public class Main {
         ArrayList<Integer> ints = new ArrayList<Integer>(Arrays.asList(0, 1, 2,
                 3, 4, 5, 6));
         System.out.println(Arrays.toString(ints.toArray()));
+        for (Integer i :
+                ints) {
+            System.out.println(i);
+        }
+        Iterator it = ints.iterator();
+        while (it.hasNext()){
+
+        }
     }
 }
